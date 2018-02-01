aacce445243ef76504c2be4075073e2787e89304
Dmitrijs Cudihins <dc@rax.lv>
2018-02-01 03:20:03 +0200

    Epsilon discount factor

--- src/solvers/value_table_policy_solver.jl
+++ src/solvers/value_table_policy_solver.jl
@@ -3,14 +3,15 @@ import POMDPs: MDP, Solver
 type ValueTablePolicySolver <: Solver
     epochs::Int64
     epsilon::Float64
+    epsilon_discount::Float64
     learning_rate::Float64
     discount::Float64
     max_frame_iterations::Int64
     print_every_n::Int64
 end
 
-ValueTablePolicySolver(epochs; epsilon = 0.2, learning_rate = 0.8, discount = 0.8, max_frame_iterations = 100, print_every_n = 25) =
-    ValueTablePolicySolver(epochs, epsilon, learning_rate, discount, max_frame_iterations, print_every_n)
+ValueTablePolicySolver(epochs; epsilon = 0.25, epsilon_discount = 0.99, learning_rate = 0.8, discount = 0.8, max_frame_iterations = 100, print_every_n = 25) =
+    ValueTablePolicySolver(epochs, epsilon, epsilon_discount, learning_rate, discount, max_frame_iterations, print_every_n)
 
 function solve(solver::ValueTablePolicySolver, pomdp::MDP; verbose = true)
 
@@ -30,9 +31,6 @@ function solve(solver::ValueTablePolicySolver, pomdp::MDP; verbose = true)
             current_action = state_actions[state_index]
             current_state = states_a[state_index]
 
-            #println("State index: $state_index, Current state: $current_action, Current actions: $current_action")
-            #println("Size: $(length(state_rewards)), $(states_b[state_index])")
-
             next_value = if isterminal(pomdp, states_b[state_index]) 0 else maximum(policy.value_map[:, states_b[state_index]]) end
             new_value = state_rewards[state_index] + solver.discount * next_value
             new_updated_value = solver.learning_rate * new_value + (1 - solver.learning_rate) * policy.value_map[current_action, current_state]
@@ -40,16 +38,16 @@ function solve(solver::ValueTablePolicySolver, pomdp::MDP; verbose = true)
             policy.value_map[current_action, current_state] = new_updated_value
         end
 
-        last_rewards[i] = sum(state_rewards)
+        if 0 < solver.epsilon_discount < 1
+            policy.epsilon *= solver.epsilon_discount
+        end
 
-        #if state_reward > 0
-        #    println("Epoch: $i, Frames: $(length(state_rewards)), Sum score: $state_reward, Max score: $(maximum(state_rewards)), Mean score: $(round(mean(state_rewards), 2)), Median score: $(median(state_rewards))")
-        #end
+        last_rewards[i] = sum(state_rewards)
 
         if verbose && (i > 100 && i % solver.print_every_n == 0)
             cum_sum_reward = sum(last_rewards[i-101:i])
             cum_avg_reward = mean(last_rewards[i-101:i])
-            println("Epoch: $i, cummulative reward: $(round(cum_avg_reward, 2))/$(round(cum_sum_reward, 2))")
+            println("Epoch: $i, Cummulative reward: $(round(cum_avg_reward, 2))/$(round(cum_sum_reward, 2)), Epsilon: $(policy.epsilon)")
         end
     end
 
--- week_2.jl
+++ week_2.jl
@@ -6,7 +6,8 @@ include("src/solvers/value_table_policy_solver.jl")
 gym_env = ToyTextMDP(gym.make("Taxi-v2"))
 #gym_env = ToyTextMDP(gym.make("FrozenLake-v0"))
 
-solver = ValueTablePolicySolver(5000; max_frame_iterations = 250, epsilon = 0.01, learning_rate = 0.85, discount = 0.99)
+#solver = ValueTablePolicySolver(5000; max_frame_iterations = 250, epsilon = 0.01, learning_rate = 0.85, discount = 0.99)
+solver = ValueTablePolicySolver(5000; max_frame_iterations = 100, epsilon = 0.25, epsilon_discount = 0.999, learning_rate = 0.15, discount = 0.99)
 policy = solve(solver, gym_env, verbose = true)
 
 # println(policy.value_map)

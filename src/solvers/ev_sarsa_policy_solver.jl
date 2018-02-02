0eb3b5e0cae950e6974eeb6d5186e7fb3f908d74
Dmitrijs Cudihins <dc@rax.lv>
2018-02-02 16:32:15 +0200

    Expected Value SARSA policy solver and minor fixes

--- src/solvers/ev_sarsa_policy_solver.jl
+++ src/solvers/ev_sarsa_policy_solver.jl
@@ -1,6 +1,6 @@
 import POMDPs: MDP, Solver
 
-type SarsaPolicySolver <: Solver
+type EVSarsaPolicySolver <: Solver
     epochs::Int64
     epsilon::Float64
     epsilon_discount::Float64
@@ -10,10 +10,10 @@ type SarsaPolicySolver <: Solver
     print_every_n::Int64
 end
 
-SarsaPolicySolver(epochs; epsilon = 0.25, epsilon_discount = 0.99, learning_rate = 0.8, discount = 0.8, max_frame_iterations = 100, print_every_n = 25) =
-    SarsaPolicySolver(epochs, epsilon, epsilon_discount, learning_rate, discount, max_frame_iterations, print_every_n)
+EVSarsaPolicySolver(epochs; epsilon = 0.25, epsilon_discount = 0.99, learning_rate = 0.8, discount = 0.8, max_frame_iterations = 100, print_every_n = 25) =
+    EVSarsaPolicySolver(epochs, epsilon, epsilon_discount, learning_rate, discount, max_frame_iterations, print_every_n)
 
-function solve(solver::SarsaPolicySolver, pomdp::MDP; verbose = true)
+function solve(solver::EVSarsaPolicySolver, pomdp::MDP; verbose = true)
 
     policy = ValueTablePolicy(pomdp, solver.epsilon, solver.learning_rate, solver.discount)
     last_rewards = zeros(Float64, solver.epochs)
@@ -37,7 +37,8 @@ function solve(solver::SarsaPolicySolver, pomdp::MDP; verbose = true)
             current_state, current_reward = generate_sr(pomdp, previous_state, current_action)
             current_state_terminal = isterminal(pomdp, current_state)
 
-            next_value = if current_state_terminal 0 else policy.value_map[action(policy, current_state), current_state] end
+            # expected value sarsa sum all values from a next state
+            next_value = if current_state_terminal 0 else  sum(policy.value_map[:, current_state]) end
             new_value = current_reward + solver.discount * next_value
             new_updated_value = solver.learning_rate * new_value + (1 - solver.learning_rate) * policy.value_map[current_action_i, previous_state]
 
--- week_3_2.jl
+++ week_3_2.jl
@@ -1,10 +1,10 @@
 include("src/base.jl")
 include("src/envs/toytext_mdp.jl")
 include("src/policies/value_table_policy.jl")
-include("src/solvers/sarsa_policy_solver.jl")
+include("src/solvers/ev_sarsa_policy_solver.jl")
 
 gym_env = ToyTextMDP(gym.make("Taxi-v2"))
-solver = SarsaPolicySolver(5000; max_frame_iterations = 100, epsilon = 0.25, epsilon_discount = 0.999, learning_rate = 0.15, discount = 0.99)
+solver = EVSarsaPolicySolver(5000; max_frame_iterations = 100, epsilon = 0.25, epsilon_discount = 0.995, learning_rate = 0.1, discount = 0.99)
 policy = solve(solver, gym_env, verbose = true)
 
 # println(policy.value_map)
